7fb09575d40a4d26a7ea2407c6c18613bba78362
nayanchauhan9999 <nayanchauhan9999@gmail.com>
2023-09-12 18:32:47 +0530

    create carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-"use client"
+"use client";
+import CarouselHomePage from "@/components/Molecules/Carousel";
 import Navbar from "@/components/Molecules/Navbar";
 
 export default function Home() {
   return (
     <main>
-      <Navbar/>
+      <Navbar />
+      <CarouselHomePage />
     </main>
   );
 }
---
+++ src/components/Molecules/Carousel/index.tsx
@@ -0,0 +1,80 @@
+import * as React from "react";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MobileStepper from "@mui/material/MobileStepper";
+import SwipeableViews from "react-swipeable-views";
+import { autoPlay } from "react-swipeable-views-utils";
+import { Toolbar } from "@mui/material";
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const images = [
+  {
+    label: "San Francisco – Oakland Bay Bridge, United States",
+    imgPath:
+      "https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60",
+  },
+  {
+    label: "Bird",
+    imgPath:
+      "https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60",
+  },
+  {
+    label: "Bali, Indonesia",
+    imgPath:
+      "https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250",
+  },
+  {
+    label: "Goč, Serbia",
+    imgPath:
+      "https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60",
+  },
+];
+
+function CarouselHomePage() {
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = images.length;
+
+  const handleStepChange = (step: number) => {
+    setActiveStep(step);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1, my: 1, px: { sx: 1, sm: 3 } }}>
+      <AutoPlaySwipeableViews
+        axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+      >
+        {images.map((step, index) => (
+          <div key={step.label}>
+            {Math.abs(activeStep - index) <= 2 ? (
+              <Box
+                component="img"
+                sx={{
+                  height: { xs: 200, md: 270 },
+                  display: "block",
+                  overflow: "hidden",
+                  width: "100%",
+                }}
+                src={step.imgPath}
+                alt={step.label}
+              />
+            ) : null}
+          </div>
+        ))}
+      </AutoPlaySwipeableViews>
+      <MobileStepper
+        steps={maxSteps}
+        position="static"
+        activeStep={activeStep}
+        nextButton={<Box sx={{ flexGrow: 1 }}></Box>}
+        backButton={<Box sx={{ flexGrow: 1 }}></Box>}
+      />
+    </Box>
+  );
+}
+
+export default CarouselHomePage;
